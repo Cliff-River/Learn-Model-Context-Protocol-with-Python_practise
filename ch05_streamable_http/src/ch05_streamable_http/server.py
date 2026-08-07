06678defd70ca37b0b211ef741d520d9ff806b4f
Cliff Huang <hzf510920@163.com>
2026-08-08 00:01:15 +0800

    Add Context notification.

--- ch05_streamable_http/src/ch05_streamable_http/server.py
+++ ch05_streamable_http/src/ch05_streamable_http/server.py
@@ -1,8 +1,15 @@
+from mcp.server.context import Context
 from mcp.server.mcpserver import MCPServer
 from typing import Optional, Any, Dict, AsyncGenerator
 
 mcp = MCPServer("Streamable Server")
 
 @mcp.tool(description="A simple tool returning file content")
-async def echo(message : str) -> str:
-    return f""
+async def echo(message : str, context : Context) -> str:
+    await context.notify("Processing file 1/3")
+    await context.notify("Processing file 2/3")
+    await context.notify("Processing file 3/3")
+    await context.notify("File processing completed")
+    return f"Here is the file conent: {message}"
+
+mcp.run(transport="streamable_http")
