f6996aa96d9be662546f0e5999bca0583a80b984
Cliff <hzf510920@163.com>
2026-08-08 14:16:15 +0800

    Testing the server with Inspector tool.

--- ch05_streamable_http/src/ch05_streamable_http/server.py
+++ ch05_streamable_http/src/ch05_streamable_http/server.py
@@ -1,15 +1,21 @@
-from mcp.server.context import Context
-from mcp.server.mcpserver import MCPServer
+import asyncio
+
+from mcp.server.mcpserver import MCPServer, Context
 from typing import Optional, Any, Dict, AsyncGenerator
 
 mcp = MCPServer("Streamable Server")
 
 @mcp.tool(description="A simple tool returning file content")
 async def echo(message : str, context : Context) -> str:
-    await context.notify("Processing file 1/3")
-    await context.notify("Processing file 2/3")
-    await context.notify("Processing file 3/3")
-    await context.notify("File processing completed")
+    await context.info("Processing file 1/3")
+    await asyncio.sleep(2)
+    await context.report_progress(2, 3, "Processing file 2/3")
+    await asyncio.sleep(2)
+    await context.report_progress(3, 3, "Processing file 3/3")
+    await asyncio.sleep(2)
+    await context.report_progress(3, 3, "File processing completed")
+    await asyncio.sleep(2)
     return f"Here is the file conent: {message}"
 
-mcp.run(transport="streamable_http")
+def main():
+    mcp.run(transport="streamable-http")
